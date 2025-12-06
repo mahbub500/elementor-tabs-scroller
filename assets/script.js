39b22d7d629fc152516e17588d9e88433c3bdc39
mahbub500 <mahbubmr500@gmaill.com>
2025-12-06 21:52:25 +0600

    arrow working

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,58 @@
-jQuery(document).ready(function ($) {
+jQuery(function ($) {
 
-    // Add arrows only once
-    if (!$('.tab-arrow').length) {
-        $('.e-n-tabs-heading').each(function () {
-            $(this).append('<div class="tab-arrow tab-arrow-left">←</div>');
-            $(this).append('<div class="tab-arrow tab-arrow-right">→</div>');
-        });
-    }
+    // 1) Prepare each tabs heading: wrap titles + inject arrows
+    $('.e-n-tabs-heading').each(function () {
+        var $heading = $(this);
 
-    // Function to switch tabs
-    function switchTab(tabButton) {
-        // Remove active state from all tabs and content panels
-        $('.e-n-tab-title').attr('aria-selected', 'false').attr('tabindex', '-1');
-        $('[id^="e-n-tab-content-"]').removeClass('e-active');
+        // Wrap all direct .e-n-tab-title in a scrollable list if not yet wrapped
+        if (!$heading.find('.e-n-tabs-heading__list').length) {
+            var $titles = $heading.children('.e-n-tab-title');
+            $titles.wrapAll('<div class="e-n-tabs-heading__list"></div>');
+        }
 
-        // Set active state to the selected tab
-        tabButton.attr('aria-selected', 'true').attr('tabindex', '0');
-        
-        // Get the corresponding content panel ID
-        let contentId = tabButton.attr('aria-controls');
-        $('#' + contentId).addClass('e-active');
+        // Add left arrow if missing
+        if (!$heading.find('.tab-arrow-left').length) {
+            $heading.prepend('<div class="tab-arrow tab-arrow-left" aria-hidden="true">←</div>');
+        }
 
-        // Scroll the tab into view
-        let container = tabButton.closest('.e-n-tabs-heading');
-        let scrollLeft = tabButton.position().left + container.scrollLeft() - 50;
-        container.animate({ scrollLeft: scrollLeft }, 300);
-    }
-
-    // Scroll left and switch to previous tab
-    $(document).on("click", ".tab-arrow-left", function () {
-        let container = $(this).closest(".e-n-tabs-heading");
-        let activeTab = container.find('.e-n-tab-title[aria-selected="true"]');
-        let prevTab = activeTab.prev('.e-n-tab-title');
-
-        if (prevTab.length) {
-            switchTab(prevTab);
+        // Add right arrow if missing
+        if (!$heading.find('.tab-arrow-right').length) {
+            $heading.append('<div class="tab-arrow tab-arrow-right" aria-hidden="true">→</div>');
         }
     });
 
-    // Scroll right and switch to next tab
-    $(document).on("click", ".tab-arrow-right", function () {
-        let container = $(this).closest(".e-n-tabs-heading");
-        let activeTab = container.find('.e-n-tab-title[aria-selected="true"]');
-        let nextTab = activeTab.next('.e-n-tab-title');
 
-        if (nextTab.length) {
-            switchTab(nextTab);
+    // 2) LEFT arrow → previous tab
+    $(document).on('click', '.tab-arrow-left', function () {
+        var $heading = $(this).closest('.e-n-tabs-heading');
+        var $list    = $heading.find('.e-n-tabs-heading__list');
+        var $active  = $list.find('.e-n-tab-title[aria-selected="true"]');
+        var $prev    = $active.prev('.e-n-tab-title');
+
+        if ($prev.length) {
+            // Scroll list to show previous tab nicely
+            var newScroll = $prev.position().left + $list.scrollLeft() - 40;
+            $list.animate({ scrollLeft: newScroll }, 250);
+
+            // Let Elementor handle tab switching
+            $prev.trigger('click');
         }
     });
 
-    // Also handle direct tab clicks to ensure proper state management
-    $(document).on("click", ".e-n-tab-title", function () {
-        switchTab($(this));
+
+    // 3) RIGHT arrow → next tab
+    $(document).on('click', '.tab-arrow-right', function () {
+        var $heading = $(this).closest('.e-n-tabs-heading');
+        var $list    = $heading.find('.e-n-tabs-heading__list');
+        var $active  = $list.find('.e-n-tab-title[aria-selected="true"]');
+        var $next    = $active.next('.e-n-tab-title');
+
+        if ($next.length) {
+            var newScroll = $next.position().left + $list.scrollLeft() - 40;
+            $list.animate({ scrollLeft: newScroll }, 250);
+
+            $next.trigger('click');
+        }
     });
 
 });
