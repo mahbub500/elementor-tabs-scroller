3bf4e6c70d3b2fd74fe89056aa04a3bb8080b8df
mahbub500 <mahbubmr500@gmaill.com>
2025-12-06 22:05:23 +0600

    arrow arrow working

--- assets/script.js
+++ assets/script.js
@@ -22,37 +22,48 @@ jQuery(function ($) {
     });
 
 
-    // 2) LEFT arrow → previous tab
-    $(document).on('click', '.tab-arrow-left', function () {
-        var $heading = $(this).closest('.e-n-tabs-heading');
-        var $list    = $heading.find('.e-n-tabs-heading__list');
-        var $active  = $list.find('.e-n-tab-title[aria-selected="true"]');
-        var $prev    = $active.prev('.e-n-tab-title');
-
-        if ($prev.length) {
-            // Scroll list to show previous tab nicely
-            var newScroll = $prev.position().left + $list.scrollLeft() - 40;
-            $list.animate({ scrollLeft: newScroll }, 250);
-
-            // Let Elementor handle tab switching
-            $prev.trigger('click');
-        }
-    });
-
-
-    // 3) RIGHT arrow → next tab
+    // 2) RIGHT arrow → next tab in loop
+    //    - "Area 1" (first position) becomes the next tab
+    //    - Current first moves to the end
     $(document).on('click', '.tab-arrow-right', function () {
         var $heading = $(this).closest('.e-n-tabs-heading');
         var $list    = $heading.find('.e-n-tabs-heading__list');
-        var $active  = $list.find('.e-n-tab-title[aria-selected="true"]');
-        var $next    = $active.next('.e-n-tab-title');
+        var $tabs    = $list.find('.e-n-tab-title');
 
-        if ($next.length) {
-            var newScroll = $next.position().left + $list.scrollLeft() - 40;
-            $list.animate({ scrollLeft: newScroll }, 250);
+        if ($tabs.length <= 1) return;
 
-            $next.trigger('click');
+        var $first  = $tabs.first();
+        var $target = $first.next('.e-n-tab-title');
+
+        // If for some reason there's only one, stay on it
+        if (!$target.length) {
+            $target = $first;
         }
+
+        // Activate the "next" tab (which will become first)
+        $target.trigger('click');   // Elementor handles aria-selected + content
+
+        // Move old first to the end => new order: [next, ..., old-first]
+        $first.appendTo($list);
+    });
+
+
+    // 3) LEFT arrow → previous tab in loop
+    //    - Last tab moves to the front and becomes active
+    $(document).on('click', '.tab-arrow-left', function () {
+        var $heading = $(this).closest('.e-n-tabs-heading');
+        var $list    = $heading.find('.e-n-tabs-heading__list');
+        var $tabs    = $list.find('.e-n-tab-title');
+
+        if ($tabs.length <= 1) return;
+
+        var $last = $tabs.last();
+
+        // Move last to the front => new order: [last, 1, 2, ...]
+        $last.prependTo($list);
+
+        // Activate the new first (previously last)
+        $last.trigger('click');     // Elementor handles aria-selected + content
     });
 
 });
